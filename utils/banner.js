201d0ceb7ac0c6c97216addbe876fdf8330cb634
VEOCE <136567088+VEOCE@users.noreply.github.com>
2025-02-24 03:33:33 +0700

    Create banner.js

---
+++ utils/banner.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+export function displayBanner() {
+  console.log(chalk.blue(`
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█ 
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█ 
+█░░║║║╠─║─║─║║║║║╠─░░█ 
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█ 
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█  
+`));
+}
